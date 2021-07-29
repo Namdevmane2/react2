03d020321e8f21295c51b2166771ee4220d713cd
Namdevmane2 <manenamdev2@gmail.com>
2021-07-29 14:01:23 +0530

    comm

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import "./App.css";
 // JSX = javascript + html
 
 function App() {
+  //
   let title = " learning React";
+
+  // stateless member
   let counter = 100;
+  let increament = () => {
+    // alert("I m Alert. ");
+    counter += 1;
+  };
   return (
     <div className="App">
-      <h1 className="bg-primary text-light p-3"> Hello world </h1>
+      <h1 className="bg-primary text-light p-3"> stateless Ui </h1>
+      {counter}
+      <input type="button" value="increment" onclick="{increment}" />
     </div>
   );
 }
