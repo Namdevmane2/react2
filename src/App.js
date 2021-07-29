cc621e263361b8123afef835f6fd67d1c60602cc
Namdevmane2 <manenamdev2@gmail.com>
2021-07-29 18:49:42 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-
+import { Helloworld } from ".src/pages/helloWorld";
 function App() {
   return (
     <div className="App">
@@ -15,15 +15,6 @@ function App() {
 
 // Tags :: Components :: Follow Pascalcase Convention
 // <Helloworld />
-function Helloworld() {
-  return (
-    <div className="bg-success p-2 m-2 border text-light">
-      <h1> Hello World</h1>
-      <hr />
-      <p> hjfcyJGDXVQYUDj jhfd HFD JHGJF jhj jgf huig kuhi jguyfe YFFE </p>
-    </div>
-  );
-}
 
 function Hello() {
   return <h1> Hello World</h1>;
---
+++ src/pages/helloWorld.js
@@ -0,0 +1,9 @@
+export function Helloworld() {
+  return (
+    <div className="bg-success p-2 m-2 border text-light">
+      <h1> Hello World</h1>
+      <hr />
+      <p> hjfcyJGDXVQYUDj jhfd HFD JHGJF jhj jgf huig kuhi jguyfe YFFE </p>
+    </div>
+  );
+}
