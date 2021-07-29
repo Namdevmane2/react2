c7c7342368ca6c691f52ac8416024e11ce733130
Namdevmane2 <manenamdev2@gmail.com>
2021-07-29 12:34:42 +0530

    log1

--- src/App.js
+++ src/App.js
@@ -4,31 +4,16 @@ import "./App.css";
 // JSX = javascript + html
 
 function App() {
-  // let id = 100;
-  // let name = "sachin";
-  // let title = " hello react ";
-  // let list = ["delhi", "mumbai", "nagpur"];
-
-  // data member
-  // let title = "Event demo";
-
-  // member function
-  // let myfun = () => {
-  //   alert("I am Click handler");
-  // };
-
-  let list = ["hello", "hello"];
-
-  const addNewHello = () => {
-    list.push("hello");
-    console.log(list);
-  };
-  // view -> that is getting render in browser.
-  // inside html you can access js variable
-
   return (
     <div className="App">
-      <h1> Working with Events and List </h1>
+      <h1> registration </h1>
+      <label> username</label>
+      <input type="text" />
+
+      <label> username</label>
+      <input type="text" />
+
+      <button type="button"> register</button>
     </div>
   );
 }
