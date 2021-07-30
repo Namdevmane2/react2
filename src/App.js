352166955e7a878d20db30f3f6a00d2877075645
Namdevmane2 <manenamdev2@gmail.com>
2021-07-30 11:35:35 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -14,10 +14,32 @@ import {
 function App() {
   return (
     <BrowserRouter>
+      <Link to="/Page1">
+        <h1>Page1</h1>
+      </Link>
+      <Link to="/Page2">
+        <h1>Page2</h1>
+      </Link>
+      <Link to="/Page3">
+        <h1>Page3</h1>
+      </Link>
+
+      <Link to="/hw">
+        <h1> Outside page1</h1>
+      </Link>
+
+      <Link to="/hu">
+        <h1> Outside page2</h1>
+      </Link>
+
+      <Link to="/Page3"> Page4</Link>
       <Route exact={true} path="/Page1" component={Page1} />
       <Route exact={true} path="/Page2" component={Page2} />
       <Route exact={true} path="/Page3" component={Page3} />
       <Route exact={true} path="/" component={Page1} />
+
+      <Route exact="true" path="/hw" component={Tag1} />
+      <Route exact="true" path="/hu" component={Tag2} />
     </BrowserRouter>
   );
 }
@@ -25,7 +47,7 @@ function App() {
 function Page1() {
   return (
     <div>
-      <h1> Page1</h1>
+      <h1> Page1 Goa </h1>
       <p>
         {" "}
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est excepturi
@@ -40,7 +62,7 @@ function Page1() {
 function Page2() {
   return (
     <div>
-      <h1> Page2</h1>
+      <h1> Page2 Mumbai</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est excepturi
         quo pariatur deleniti rerum quae amet iste, cumque harum voluptatem,
@@ -54,7 +76,7 @@ function Page2() {
 function Page3() {
   return (
     <div>
-      <h1> Page3</h1>
+      <h1> Page3 Nashik </h1>
       <p>
         {" "}
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est excepturi
